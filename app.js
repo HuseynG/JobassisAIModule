4b683e8b495df2c8b063d663b7da3d51be17b1f0
HuseynG <huseyndev@gmail.com>
2023-08-27 16:44:56 +0100

    cors?

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
 
 const app = express();
 const port = 8080;
